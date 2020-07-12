b0efc49962defeeb5d2a47bba5b74b58f4e53c3a
Seonghyeon Kim <self@seonghyeon.dev>
2020-07-12 18:48:41 +0900

    Update script

--- .bin/script.sh
+++ .bin/script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 git log --name-status HEAD~20..HEAD --pretty=format: . | awk '$1 == "M" || $1 == "A" { print }' | python .bin/recent_changes_updater.py > README.md
+if git add README.md; then git commit -m "[Auto] Update recent changes"; fi
